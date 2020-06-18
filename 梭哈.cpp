7d6c7db48baa689e3b803bc1c008f6a01bb4c695
johnson-tai <66660394+johnson-tai@users.noreply.github.com>
2020-06-18 18:17:54 +0800

    A7222281
    梭哈排組

---
+++ 梭哈.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+	vector <string>v;
+	v.push_back("\6 A ");
+	v.push_back("\3 A ");
+	v.push_back("\5 A ");
+	v.push_back("\4 A ");
+	v.push_back("\6 2 ");
+	v.push_back("\3 2 ");
+	v.push_back("\5 2 ");
+	v.push_back("\4 2 ");
+	v.push_back("\6 3 ");
+	v.push_back("\3 3 ");
+	v.push_back("\5 3 ");
+	v.push_back("\4 3 ");
+	v.push_back("\6 4 ");
+	v.push_back("\3 4 ");
+	v.push_back("\5 4 ");
+	v.push_back("\4 4 ");
+	v.push_back("\6 5 ");
+	v.push_back("\3 5 ");
+	v.push_back("\5 5 ");
+	v.push_back("\4 5 ");
+	v.push_back("\6 6 ");
+	v.push_back("\3 6 ");
+	v.push_back("\5 6 ");
+	v.push_back("\4 6 ");
+	v.push_back("\6 7 ");
+	v.push_back("\3 7 ");
+	v.push_back("\5 7 ");
+	v.push_back("\4 7 ");
+	v.push_back("\6 8 ");
+	v.push_back("\3 8 ");
+	v.push_back("\5 8 ");
+	v.push_back("\4 8 ");
+	v.push_back("\6 9 ");
+	v.push_back("\3 9 ");
+	v.push_back("\5 9 ");
+	v.push_back("\4 9 ");
+	v.push_back("\6 10 ");
+	v.push_back("\3 10 ");
+	v.push_back("\5 10 ");
+	v.push_back("\4 10 "); 
+	v.push_back("\6 J ");
+	v.push_back("\3 J ");
+	v.push_back("\5 J ");
+	v.push_back("\4 J ");
+	v.push_back("\6 Q ");
+	v.push_back("\3 Q ");
+	v.push_back("\5 Q ");
+	v.push_back("\4 Q ");
+	v.push_back("\6 K "); 
+	v.push_back("\3 K ");
+	v.push_back("\5 K ");
+	v.push_back("\4 K ");
+	
+	
+	for(int i=0;i<52;i++)
+	    cout<<v[i]<<" ";
+}
