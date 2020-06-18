3df45c4c8690ee5cd9a04e71bf3a73b9ee5b0828
TimLiu1987 <59197061+TimLiu1987@users.noreply.github.com>
2020-06-18 13:33:23 +0800

    Add files via upload

---
+++ A8250343-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "A8250343.h"
+using namespace std;
+
+A8250343::playercard(double card)
+{
+    card = //platercard;
+}
+
+void A8250343::printCard()
+{
+    cout <<　card << endl;
+}
+
+void A8250343::setplayerCard(double _n)
+{
+    _n = 0;
+    card = _n;
+    return card;
+}
+
+void A8250343::print()
+{
+    cout << card <<endl;
+}
---
+++ A8250343.h
@@ -0,0 +1,12 @@
+#ifndef A8250343_H
+#define A8250343_H
+
+class A8250343 : public //包含玩家手牌的程式
+{
+    public:
+        playercard(double);
+        void printCard();
+        void setplayerCard(double);
+        void print();
+};
+#endif
